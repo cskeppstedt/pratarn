434927f6af468400259382d0dc9aefe7e93b3adf
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2018-12-07 23:25:24 +0100

    Nicer logging

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,17 +1,15 @@
 import { config, createLogger, format, transports } from "winston";
 
 const consoleTransport = new transports.Console({
-  handleExceptions: true
-});
-
-const fileTransport = new transports.File({
-  dirname: "logs",
-  filename: "pratarn.log",
-  maxFiles: 7,
-  maxsize: 1024 * 1024 * 30
+  handleExceptions: true,
+  format: format.combine(
+    format.colorize(),
+    format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    format.printf((info) => `${info.timestamp} [${info.level}] - ${info.message}`)
+  )
 });
 
 export default createLogger({
   level: process.env.LOG_LEVEL || "verbose",
-  transports: [consoleTransport, fileTransport]
+  transports: [consoleTransport]
 });
