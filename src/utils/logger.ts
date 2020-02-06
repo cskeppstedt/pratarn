6b59ac3a67a9fde7f1bdb6449596fb12c9e93eea
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2020-02-07 00:07:37 +0100

    Rotating log files

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,4 +1,18 @@
 import { createLogger, format, transports } from 'winston';
+import 'winston-daily-rotate-file';
+
+const rotateFileTransport = new (transports.DailyRotateFile)({
+  dirname: 'logs',
+  filename: 'pratarn-%DATE%.log',
+  datePattern: 'YYYY-MM-DD-HH',
+  zippedArchive: true,
+  maxSize: '20m',
+  maxFiles: '7d',
+  format: format.combine(
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    format.printf((info) => `${info.timestamp} [${info.level}] - ${info.message}`),
+  ),
+});
 
 const consoleTransport = new transports.Console({
   handleExceptions: true,
@@ -11,5 +25,5 @@ const consoleTransport = new transports.Console({
 
 export default createLogger({
   level: process.env.LOG_LEVEL || 'verbose',
-  transports: [consoleTransport],
+  transports: [consoleTransport, rotateFileTransport],
 });
