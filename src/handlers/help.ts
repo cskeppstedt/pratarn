946c5bf6929c8a40a6fe5c2d5d02f887d073df0e
Christoffer Skeppstedt <c.skeppstedt@gmail.com>
2020-02-06 23:47:16 +0100

    Include version info

--- src/handlers/help.ts
+++ src/handlers/help.ts
@@ -1,9 +1,11 @@
 import { IHandler } from '../types';
 import isBot from '../utils/is_bot';
 
-const describeCommands = (handlers: IHandler[]) => `Available commands:\n\n${handlers
-  .map(({ command, description }) => `**${command}**\n${description}`)
-  .join('\n\n')}`;
+const describeCommands = (handlers: IHandler[]) => {
+  const handlerDescriptions = handlers
+    .map(({ command, description }) => `**${command}**\n${description}`);
+  return ['Pratarn (discord.js)', 'Available commands:', ...handlerDescriptions].join('\n\n');
+};
 
 export default (handlers: IHandler[]) => ({
   command: '!help',
