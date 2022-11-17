b607f2c9f580bdae198e2a55c7c52f94a04979c8
Christoffer <c.skeppstedt@gmail.com>
2022-11-17 22:45:10 +0100

    Bump deps and use new dynamo sdk client

--- src/utils/dynamo.ts
+++ src/utils/dynamo.ts
@@ -1,34 +1,42 @@
-import AWS from 'aws-sdk';
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  BatchWriteCommand,
+  DynamoDBDocumentClient,
+  PutCommand,
+  QueryCommand,
+} from "@aws-sdk/lib-dynamodb";
 import {
   IFetchMessageObjectsResponse,
   IStorageMessageView,
   NormalizedUsername,
-} from '../types';
-import MemoryCache from './memory_cache';
+} from "../types";
+import MemoryCache from "./memory_cache";
 
-require('dotenv').config();
+require("dotenv").config();
 
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_DEFAULT_REGION,
-});
+const documentClient = DynamoDBDocumentClient.from(
+  new DynamoDBClient({
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    },
+    region: process.env.AWS_DEFAULT_REGION,
+  })
+);
 
 const messagesCache = new MemoryCache(60 * 5); // 5 min cache
 
-const TABLE_NAME = 'PratarnMessagesObjectsTest';
+const TABLE_NAME = "PratarnMessagesObjectsTest";
 
 export const insertMessageObject = (messageObject: IStorageMessageView) => {
-  const documentClient = new AWS.DynamoDB.DocumentClient();
   const params = {
     TableName: TABLE_NAME,
     Item: messageObject,
   };
-  return documentClient.put(params).promise();
+  return documentClient.send(new PutCommand(params));
 };
 
 export const insertMessageObjects = (messageObjects: IStorageMessageView[]) => {
-  const documentClient = new AWS.DynamoDB.DocumentClient();
   const params = {
     RequestItems: {
       [TABLE_NAME]: messageObjects.map((messageObject) => ({
@@ -36,24 +44,24 @@ export const insertMessageObjects = (messageObjects: IStorageMessageView[]) => {
       })),
     },
   };
-  return documentClient.batchWrite(params).promise();
+  return documentClient.send(new BatchWriteCommand(params));
 };
 
 export const fetchMessageObjects = async (username: NormalizedUsername) => {
-  const documentClient = new AWS.DynamoDB.DocumentClient();
   const params = {
     TableName: TABLE_NAME,
-    KeyConditionExpression: 'author_username = :author_username',
-    ExpressionAttributeValues: { ':author_username': username },
+    KeyConditionExpression: "author_username = :author_username",
+    ExpressionAttributeValues: { ":author_username": username },
   };
 
-  const response = await documentClient.query(params).promise();
+  const response = await documentClient.send(new QueryCommand(params));
   return {
     count: response.Count as number,
     messageObjects: response.Items as IStorageMessageView[],
   } as IFetchMessageObjectsResponse;
 };
 
-export const fetchMessageObjectsCached = async (username: NormalizedUsername) => messagesCache.get(username, () => fetchMessageObjects(username)) as Promise<
-IFetchMessageObjectsResponse
->;
+export const fetchMessageObjectsCached = async (username: NormalizedUsername) =>
+  messagesCache.get(username, () =>
+    fetchMessageObjects(username)
+  ) as Promise<IFetchMessageObjectsResponse>;
